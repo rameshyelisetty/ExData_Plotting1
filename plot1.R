a9f7af893159fab266ec9a46efe9de5dbb59a258
rameshyelisetty <ramesh_yelisetty@yahoo.com>
2015-09-13 10:51:25 -0700

    initial plot1

---
+++ plot1.R
@@ -0,0 +1,34 @@
+# Initial Code, with library("sqldf"), and use read.csv.sql; but, this function doesnt seem to work with na.strings = "?"
+# so... explore later
+# get back to simple read.csv, computer has enough memory to handle the records
+
+# DATA PREPERATION
+
+# Load data from file, create initial dataset - dataSet0
+powerDataFile <- "household_power_consumption.txt"
+dataSet0 <- read.csv(powerDataFile,sep=";",header=T, na.strings="?")
+
+# Filter the data set to the given date criteria - 1/2/2007 and 2/2/2007
+dataSet1 = dataSet0[dataSet0$Date == '2/2/2007' | dataSet0$Date == '1/2/2007',]
+
+# get rid of the initial dataset
+rm(dataSet0)
+
+# Convert date and time formats
+dataSet1 <- transform(dataSet1, Date = as.Date(Date,"%d/%m/%Y"), Time = format(strptime(Time, format="%H:%M:%S"),format="%H:%M:%S"))
+
+# DATA-FRAME READY
+
+# PROCESS
+
+# plot1 - Histogram
+hist(dataSet1$Global_active_power,
+col="red",
+ylim=c(0,1200),
+xlab="Global Active Power(killowatts)",
+main="Global Active Power")
+
+# OUTPUT TO DEVICE
+
+dev.copy(png, file="plot1.png") ## Copy to png device/file
+dev.off() ## Close the device
